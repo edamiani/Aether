9009f75d14fc6f52b2d28ae8cbf05e737a03078f
Edgard B. Damiani <edamiani@gmail.com>
2015-01-24 17:32:07 -0200

    First commit

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public int test;
+
+	private static GameManager Instance;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if(Instance == null)
+		{
+			//If I am the first instance, make me the Singleton
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			//If a Singleton already exists and you find
+			//another reference in scene, destroy it!
+			if(this != Instance)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
