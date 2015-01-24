ab3b381dce5d7d0d6ce65b0c39966fb9fb43b4b3
Edgard B. Damiani <edamiani@gmail.com>
2015-01-24 20:54:25 -0200

    - Added GameManager. - Added scene stubs.

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -4,7 +4,32 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	public int test;
 
-	private static GameManager Instance;
+	public static GameManager	Instance;
+
+	struct Score
+	{
+		int air;
+		int earth;
+		int fire;
+		int water;
+	}
+
+	public enum ElementalType : int
+	{
+		Air = 0,
+		Earth = 1,
+		Fire = 2,
+		Water = 3,
+		Aether = 4,
+		
+		None
+	}
+
+	public ElementalType		initialElement { get { return mInitialElement; } set { mInitialElement = value; } }
+	public Score				score { get { return mScore; } }
+
+	private ElementalType		mInitialElement;
+	private Score				mScore;
 
 	// Use this for initialization
 	void Awake () 
@@ -30,5 +55,10 @@ public class GameManager : MonoBehaviour {
 	void Update () 
 	{
 	
+	}
+
+	public void LoadMiniGame(ElementalType game1, ElementalType game2 = ElementalType.None)
+	{
+		
 	}
 }
---
+++ Assets/Code/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void LoadScene(int elementalType)
+	{
+		GameManager.Instance.initialElement = (GameManager.ElementalType) elementalType;
+
+		GameManager.Instance.test = elementalType;
+
+		Application.LoadLevel(2);
+	}
+}
