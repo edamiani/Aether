6d73929c27eb0fbbb657e8be5d7d663d949ed936
gregoriobenatti <gregoriobenatti@gmail.com>
2015-01-25 16:29:52 -0200

    FadeIn and FadeOut

---
+++ Assets/Code/FadeScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeScreen : MonoBehaviour 
+{
+	
+	Image img;
+	public float duration = 2f;
+
+	void Awake () 
+	{
+		img = GetComponent<Image>();
+		img.color = new Color(0, 0, 0, 1);
+		FadeIn ();
+	}
+	
+	public void FadeOut()
+	{
+		img.color = new Color(1, 1, 1, 0);
+		img.DOFade(1, duration);
+	}
+	
+	public void FadeIn()
+	{
+		img.DOFade(0, duration);
+	}
+}
+
--- Assets/Code/SceneManagement/SceneManagerAether.cs
+++ Assets/Code/SceneManagement/SceneManagerAether.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 class SceneManagerAether : SceneManager
 {
+	public FadeScreen fade;
+
 	void Awake()
 	{
 		Debug.Log("Game Over!!!");
+		Invoke ("FadeOutNow", 5f);
+	}
+
+	public void FadeOutNow ()
+	{
+		fade.FadeOut ();
 	}
 
 	public override void LoadScene(int sceneId)
 	{
-		
+
 	}
 
 	void Update()
