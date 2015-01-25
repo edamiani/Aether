80b243a199127b1c91e62ee080a254675a96b565
regisclaus <regis.claus@gmail.com>
2015-01-25 15:57:54 -0200

    Characater Animation

---
+++ Assets/Code/Character/Character.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	private float maxSpeed = 20f;
+
+	private bool facingLeft = true;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		float moveX = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity= new Vector2 (moveX * maxSpeed, rigidbody2D.velocity.y);
+
+		anim.SetFloat ("Speed", Mathf.Abs (moveX));
+
+		if(moveX < 0 && !facingLeft) {
+			Flip();
+		}
+		else {
+			if(moveX  > 0 && facingLeft) {
+				Flip();
+			}
+		}
+
+	}
+
+	void Flip() {
+		facingLeft = !facingLeft;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
