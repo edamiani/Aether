fabbaed0923fae364cf9fcbfa6734251fd8e16c3
gregoriobenatti <gregoriobenatti@gmail.com>
2015-01-25 10:14:28 -0200

    Add itens to NavMesh working

---
+++ Assets/Code/AirMinigameBlockPosition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirMinigameBlockPosition : MonoBehaviour {
+
+	public Transform blockPoint;
+	public GameObject goal;
+
+	bool isAdded = false;
+	
+	void OnMouseUp()
+	{
+		// call method that creates a 'path blocker'
+		AddPathBlocker ();
+	}
+
+	void OnMouseOver()
+	{
+		//Debug.Log ("OnMouseOver");
+	}
+
+	// Add a 'pathblocker' to the navmesh
+	void AddPathBlocker()
+	{
+		if (!isAdded)
+		{
+			Vector3 v3 = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+			Transform block = Instantiate(blockPoint, v3, Quaternion.identity) as Transform;
+			Debug.Log("ooook") ;
+			isAdded = true;
+		}
+		else
+		{
+//			DestroyImmediate(blockPoint, true);
+			Debug.Log("NOTTTTT ooook") ;
+			isAdded = false;
+		}
+
+	}
+}
+
