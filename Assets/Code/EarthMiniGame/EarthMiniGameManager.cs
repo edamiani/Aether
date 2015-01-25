69b92d012c3491fd1ae2d04005fb6b2b386eb1e5
regisclaus <regis.claus@gmail.com>
2015-01-25 04:57:23 -0200

    Respawn enemies in random position

---
+++ Assets/Code/EarthMiniGame/EarthMiniGameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthMiniGameManager : MonoBehaviour {
+
+	private static int 	MAX_ENEMIES = 9;
+
+	public Enemy[]		enemies = new Enemy[MAX_ENEMIES];
+
+	private float 		timerToRespawn;
+	private int 		waitingTimeToRespawn = 2;
+
+	private int			timeToHide = 2;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.CanRespawnEnemy ();
+	
+	}
+
+	private void CanRespawnEnemy() {
+		timerToRespawn += Time.deltaTime;
+		if(timerToRespawn > waitingTimeToRespawn){
+			bool isEnemyRespawned = this.TryRespawnRandomEnemy();
+			if(isEnemyRespawned) {
+				timerToRespawn = 0;
+			}
+		}
+	}
+
+	private bool TryRespawnRandomEnemy() {
+		int indexRandomEnemy = Random.Range(0, MAX_ENEMIES);
+		Enemy enemyToRespawn = enemies [indexRandomEnemy];
+		if (!enemyToRespawn.respawned) {
+			enemyToRespawn.Respawn(timeToHide);
+			return true;
+		}
+		return false;
+
+	}
+}
---
+++ Assets/Code/EarthMiniGame/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private float 		timeToHide = 0;
+	private int			waitingTimeToHide;
+
+	private bool		mRespawned = false;
+	public bool			respawned { get { return mRespawned; } }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CanHide ();
+		
+	}
+
+	private void CanHide() {
+
+		if (this.mRespawned) {
+			timeToHide += Time.deltaTime;
+			if(this.timeToHide > this.waitingTimeToHide){
+				this.Hide();
+				timeToHide = 0;
+			}
+		}
+	}
+
+	public void Hide() {
+		this.renderer.enabled = false;
+		this.mRespawned = false;
+	}
+
+	public void Respawn(int waitingTimeToHide) {
+		this.renderer.enabled = true;
+		this.waitingTimeToHide = waitingTimeToHide;
+		this.mRespawned = true;
+	}
+}
